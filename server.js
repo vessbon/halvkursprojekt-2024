46ad38c8642ebd4dc48eeb448381d0bf0716c77d
Anton Norgren <antnor0615@edu.halmstad.se>
2024-12-13 13:31:31 +0100

    Fixed permissions

--- server.js
+++ server.js
@@ -130,7 +130,7 @@ async function index(req, res) {
                         <p>Uploaded on ${new Date(video.date).toLocaleDateString('en-EN', dateOptions)} <strong>By ${video.username}</strong></p>
                         `;
 
-                    if (req.session.userId == video.id) {
+                    if (req.session.userId == video.id || req.session.admin) {
                         send += `
                         <a href="/delete/${video.watch_code}">
                             <svg class="delete-btn" xmlns = "http://www.w3.org/2000/svg" viewBox = "0 0 448 512">< !--!Font Awesome Free 6.7.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="currentColor" d="M135.2 17.7L128 32 32 32C14.3 32 0 46.3 0 64S14.3 96 32 96l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-96 0-7.2-14.3C307.4 6.8 296.3 0 284.2 0L163.8 0c-12.1 0-23.2 6.8-28.6 17.7zM416 128L32 128 53.2 467c1.6 25.3 22.6 45 47.9 45l245.8 0c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>
@@ -142,7 +142,7 @@ async function index(req, res) {
 
                     return send;
                 } else {
-                    return `<p> Unsupported file type: ${video.filename}</p> `;
+                    return `<p>Unsupported file type: ${video.filename}</p>`;
                 }
             }).join('') // Adds upp all the video html as one string
 
